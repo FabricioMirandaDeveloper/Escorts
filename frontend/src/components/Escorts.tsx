a7fb0995c605974954ac136f1f13f0fc146b99ae
Fabricio Miranda <mirandaquezadafabriciojoel@gmail.com>
2024-10-10 00:26:43 -0500

    Configuración inicial de interfaz y estilos con Tailwind para mostrar usuario

--- frontend/src/components/Escorts.tsx
+++ frontend/src/components/Escorts.tsx
@@ -1,27 +1,44 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+interface Usuario {
+    id: number;
+    nombre: string;
+    edad: number;
+    celular: string;
+    pais: string;
+    email: string;
+    password_hash: string; // O cualquier otro campo que necesites
+    imagenes: string; // La propiedad que almacena la URL de la imagen
+}
 
 export default function Escorts() {
+    const [usuarios,setUsuarios] = useState<Usuario[]>([])
     useEffect(() => {
         const fetchEscorts = async () => {
             try {
-                const response = await fetch('http://localhost:3000/usuarios'); 
+                const response = await fetch('http://localhost:3000/usuarios')
                 if (!response.ok) {
-                    throw new Error('Error en la solicitud');
+                    throw new Error('Error en la solicitud')
                 }
-                const data = await response.json(); 
+                const data = await response.json()
                 console.log(data);
                 
+                setUsuarios(data)
             } catch (error) {
-                console.error('Error al obtener los datos:', error);
+                console.error('Error al obtener los datos:', error)
             }
         }
         fetchEscorts()
     }, [])
     return(
         <main>
-            <section>
-
+            <section className="grid grid-cols-3 gap-1">
+                {usuarios.map(usuario => (
+                    <div key={usuario.id}>
+                        <img src={usuario.imagenes} alt={usuario.nombre} className="h-48 object-cover rounded-sm"/>
+                    </div>
+                ))}
             </section>
         </main>
     )
-}
+}   
